2f98fe7274ebf6ca1ffe35fcbe55dc7ed876af5b
MrSaints <ian@fyianlai.com>
2014-03-22 16:46:38 +0000

    Wrapped resize function in Foundation's throttle

--- assets/js/application.js
+++ assets/js/application.js
@@ -1,18 +1,16 @@
 (function ($) {
     $(document).foundation();
 
+    var $window = $(window);
     var $vAlign_padding = 0;
     var $vAlign_obj = $('.js-valign');
 
     var $vAlign = function () {
-        $vAlign_padding = ($(window).height() - $vAlign_obj.height() - 75) / 2;
-        if ($vAlign_padding < 100) $vAlign_padding = 100;
+        $vAlign_padding = ($window.height() - $vAlign_obj.height() - 75) / 2;
+        if ($vAlign_padding <= 100) $vAlign_padding = 100;
         $vAlign_obj.css('padding', $vAlign_padding + 'px 0');
     }
     $vAlign();
 
-    $(window).on('resize', Foundation.utils.throttle(function(e) {
-        $vAlign();
-    }, 300));
-
+    $window.on('resize', Foundation.utils.throttle($vAlign, 300));
 })(jQuery);
