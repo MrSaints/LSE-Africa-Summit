60721e6bbb7e48643be2b573f807717e19e5c6ed
MrSaints <ian@fyianlai.com>
2014-03-22 20:24:33 +0000

    Added footer, Twitter feed w/ Morphist's Animate.css rotations, and links

--- assets/js/application.js
+++ assets/js/application.js
@@ -13,4 +13,17 @@
     $vAlign();
 
     $window.on('resize', Foundation.utils.throttle($vAlign, 300));
+
+    $.getJSON("http://twitcher.steer.me/user_timeline/LSEAfricaSummit?key=kc2tcvkc", function (data) {
+        $tweets = '';
+        for (var i = data.length - 1; i >= 0; i--) {
+            $tweets += '<li><h6 class="twitter-posted">' + moment(data[i].created_at).fromNow() + '</h6>';
+            $tweets += '<h4 class="twitter-tweet">' + data[i].text + '</h4></li>';
+        };
+        $('#js-twitter').html($tweets).Morphist({
+            animateIn: "fadeInLeft",
+            animateOut: "fadeOutRight",
+            speed: 3500
+        });
+    });
 })(jQuery);
