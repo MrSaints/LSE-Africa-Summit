a3fbe7cbecc46d08a49ca517630b83559890e5dc
MrSaints <ian@fyianlai.com>
2014-03-23 06:09:38 +0000

    Fixed image URLs and replaced static title with blog name

--- front-page.php
+++ front-page.php
@@ -2,7 +2,7 @@
 
 <div class="row text-center hero js-valign">
     <div class="large-8 large-centered columns">
-        <h1 class="hero-title">LSE Africa Summit</h1>
+        <h1 class="hero-title"><?php bloginfo( 'name' ); ?></h1>
         <h2 class="hero-subtitle">4 &ndash; 5th April 2014, London</h2>
         <a href="#" class="button hero-action">Register Now</a>
     </div>
@@ -14,14 +14,14 @@
     </div>
     <div class="large-10 columns">
         <ul class="contributors-list large-block-grid-8 medium-block-grid-4 small-block-grid-2">
-            <li><a href="//www.zenithbank.com" target="_blank"><img src="assets/img/logos/Zenith.png" alt=""></a></li>
-            <li><a href="//www.prudential.co.uk" target="_blank"><img src="assets/img/logos/Prudential.png" alt=""></a></li>
-            <li><a href="//www.africa.airtel.com/wps/wcm/connect/africarevamp/Nigeria/" target="_blank"><img src="assets/img/logos/Airtel.png" alt=""></a></li>
-            <li><a href="//www.lse.ac.uk/internationalDevelopment/home.aspx" target="_blank"><img src="assets/img/logos/LSE-ID.png" alt=""></a></li>
-            <li><a href="//www.africanbusinessmagazine.com" target="_blank"><img src="assets/img/logos/Africa-Business.png" alt=""></a></li>
-            <li><a href="//www.afrimind.org" target="_blank"><img src="assets/img/logos/AfriMind.png" alt=""></a></li>
-            <li><a href="//www.eaecc.com" target="_blank"><img src="assets/img/logos/AllAfrica.png" alt=""></a></li>
-            <li><a href="//www.thisisafricaonline.com" target="_blank"><img src="assets/img/logos/This-is-Africa.png" alt=""></a></li>
+            <li><a href="//www.zenithbank.com" target="_blank"><img src="<?php echo lseafricasummit_images() ?>/logos/Zenith.png" alt=""></a></li>
+            <li><a href="//www.prudential.co.uk" target="_blank"><img src="<?php echo lseafricasummit_images() ?>/logos/Prudential.png" alt=""></a></li>
+            <li><a href="//www.africa.airtel.com/wps/wcm/connect/africarevamp/Nigeria/" target="_blank"><img src="<?php echo lseafricasummit_images() ?>/logos/Airtel.png" alt=""></a></li>
+            <li><a href="//www.lse.ac.uk/internationalDevelopment/home.aspx" target="_blank"><img src="<?php echo lseafricasummit_images() ?>/logos/LSE-ID.png" alt=""></a></li>
+            <li><a href="//www.africanbusinessmagazine.com" target="_blank"><img src="<?php echo lseafricasummit_images() ?>/logos/Africa-Business.png" alt=""></a></li>
+            <li><a href="//www.afrimind.org" target="_blank"><img src="<?php echo lseafricasummit_images() ?>/logos/AfriMind.png" alt=""></a></li>
+            <li><a href="//www.eaecc.com" target="_blank"><img src="<?php echo lseafricasummit_images() ?>/logos/AllAfrica.png" alt=""></a></li>
+            <li><a href="//www.thisisafricaonline.com" target="_blank"><img src="<?php echo lseafricasummit_images() ?>/logos/This-is-Africa.png" alt=""></a></li>
         </ul>
     </div>
 </div>
